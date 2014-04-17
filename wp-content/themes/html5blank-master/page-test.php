f958097200bfda5e0d092a05073475c8a680a1cc
nicos-hub <nico@dolisy.fr>
2014-04-17 16:25:22 +0200

    avec page-test

---
+++ wp-content/themes/html5blank-master/page-test.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html lang="fr-FR" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+
+		<title> FIST</title>
+
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Just another WordPress site">
+
+		<link rel='stylesheet' id='normalize-css'  href='http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/normalize.css?ver=1.0' media='all' />
+		<link rel='stylesheet' id='html5blank-css'  href='http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/style.css?ver=1.0' media='all' />
+		<script type='text/javascript' src='http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/js/lib/conditionizr-4.3.0.min.js?ver=4.3.0'></script>
+		<script type='text/javascript' src='http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/js/lib/modernizr-2.7.1.min.js?ver=2.7.1'></script>
+		<script type='text/javascript' src='http://localhost/~nicolasdolisy/FIST/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
+		<script type='text/javascript' src='http://localhost/~nicolasdolisy/FIST/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+		<script type='text/javascript' src='http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master/js/scripts.js?ver=1.0.0'></script>
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: 'http://localhost/~nicolasdolisy/FIST/wp-content/themes/html5blank-master',
+            tests: {}
+        });
+        </script>
+
+	</head>
+
+	<body class="home logged-in">
+		<div class="zoom">
+			<p>
+				<img src="./img/fist.jpg"> 
+			</p>
+		</div>
+		<div class="menu">
+			<ul>
+			<li><a href="#whoami" id="link1">Qui sommes nous</a></li>
+			<li><a href="#services" id="link4">Services</a></li>
+			<li><a href="#promo" id="link2">Nos promotions</a></li>
+			<li><a href="#lieu" id="link5">Nous trouver</a></li>
+			<li><a href="#contact" id="link3">Contacts</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
